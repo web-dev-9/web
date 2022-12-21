013fcb06abeb1f8dcce8397a569a3493bce3caae
rathan <4.0webdev@gmail.com>
2022-12-21 16:59:09 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Style Sheet URL:</h1>
+      <p>Copy the below URL and Paste it at the top of the CSS file</p>
+      <textarea rows="10" cols="40">
+        @import
+        url('https://fonts.googleapis.com/css2?family=Bree+Serif&family=Caveat:wght@400;700&family=Lobster&family=Monoton&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Playfair+Display+SC:ital,wght@0,400;0,700;1,700&family=Playfair+Display:ital,wght@0,400;0,700;1,700&family=Roboto:ital,wght@0,400;0,700;1,400;1,700&family=Source+Sans+Pro:ital,wght@0,400;0,700;1,700&family=Work+Sans:ital,wght@0,400;0,700;1,700&display=swap');
+      </textarea>
+      <h2>Reference Image:</h2>
+      <img
+        src="https://res.cloudinary.com/dwwimfezn/image/upload/v1671639062/Screenshot_513_mevfzg.png"
+        alt="ref"
+      />
     </div>
   );
 }
